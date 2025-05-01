b9f0816cc8abd1a479b13bf9aac1f1402b5e150e
Antoine <me@antoinebellanger.fr>
2025-05-01 12:07:38 +0200

    feat(math): DynamicMatrix2D
    Signed-off-by: Antoine <me@antoinebellanger.fr>

--- src/math/Matrix.hpp
+++ src/math/Matrix.hpp
@@ -36,4 +36,35 @@ namespace raytracer::math
         private:
             std::array<std::array<K, sizeY>, sizeX> _2DArray;
     };
+
+    template <typename K>
+    class DynamicMatrix2D
+    {
+        public:
+            DynamicMatrix2D() = delete;
+            explicit DynamicMatrix2D(const std::size_t sizeX, const std::size_t sizeY)
+                : _sizeX(sizeX), _sizeY(sizeY)
+            {
+                _2DArray.resize(sizeX, std::vector<K>(sizeY));
+            }
+
+            K& operator()(std::size_t x, std::size_t y)
+            {
+                if (x >= _sizeX || y >= _sizeY)
+                    throw std::out_of_range("Matrix2D index out of bounds");
+                return _2DArray[x][y];
+            }
+
+            const K& operator()(std::size_t x, std::size_t y) const
+            {
+                if (x >= _sizeX || y >= _sizeY)
+                    throw std::out_of_range("Matrix2D index out of bounds");
+                return _2DArray[x][y];
+            }
+
+        private:
+            std::vector<std::vector<K>> _2DArray;
+            std::size_t _sizeX;
+            std::size_t _sizeY;
+    };
 }
