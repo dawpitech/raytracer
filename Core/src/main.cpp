d8a8048ab2e86eb9c555ef3f93ac6c48566b069c
Antoine <me@antoinebellanger.fr>
2025-05-06 16:09:34 +0200

    feat: add argument handling
    Signed-off-by: Antoine <me@antoinebellanger.fr>

--- Core/src/main.cpp
+++ Core/src/main.cpp
@@ -5,14 +5,24 @@
 ** main.cpp
 */
 
+#include <iostream>
+
 #include "Raytracer.hpp"
 
-int main()
+static void printHelp() { std::cout << "USAGE: ./raytracer <SCENE_FILE>\n\tSCENE_FILE: scene configuration" << std::endl; }
+
+int main(const int argc, const char** argv)
 {
     raytracer::Raytracer raytracer;
 
+    if (argc != 2)
+        return printHelp(), 84;
+    const std::string arg(argv[1]);
+    if (arg == "-help" || arg == "--help" || arg == "-h")
+        return printHelp(), 0;
+
     raytracer.loadPlugins();
-    raytracer.parseSceneConfig("./scenes/default.cfg");
+    raytracer.parseSceneConfig(arg);
     raytracer.run();
     return 0;
 }
